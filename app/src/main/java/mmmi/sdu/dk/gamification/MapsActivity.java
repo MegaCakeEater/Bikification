2d9991b7ed774c4b902a23d65a8077c3ecce9b29
Bogs <Emil_ngaard@hotmail.com>
2017-10-09 17:31:26 +0200

    Stole a bunch of stuff of StackOverflow

--- app/src/main/java/mmmi/sdu/dk/gamification/MapsActivity.java
+++ app/src/main/java/mmmi/sdu/dk/gamification/MapsActivity.java
@@ -1,43 +1,58 @@
 package mmmi.sdu.dk.gamification;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.DirectionsApi;
 
+import mmmi.sdu.dk.gamification.routing.GetDirectionsAsync;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+      private GoogleMap mMap;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                  .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+      }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-    }
+      /**
+       * Manipulates the map once available.
+       * This callback is triggered when the map is ready to be used.
+       * This is where we can add markers or lines, add listeners or move the camera. In this case,
+       * we just add a marker near Sydney, Australia.
+       * If Google Play services is not installed on the device, the user will be prompted to install
+       * it inside the SupportMapFragment. This method will only be triggered once the user has
+       * installed Google Play services and returned to the app.
+       */
+      @Override
+      public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+            LatLng[] params = new LatLng[2];
+            LatLngBounds bounds = new LatLngBounds(new LatLng(55.37067460000001, 10.428067300000066),  new LatLng(55.37545296581934,10.428192615509033));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 400, 400, 0));
+            params[0] = new LatLng(55.37067460000001, 10.428067300000066);
+            params[1] = new LatLng(55.37545296581934,10.428192615509033);
+            AsyncTask at = new GetDirectionsAsync(this.mMap);
+            at.execute(params);
+      }
+
 }
---
+++ app/src/main/java/mmmi/sdu/dk/gamification/routing/GetDirectionsAsync.java
@@ -0,0 +1,122 @@
+package mmmi.sdu.dk.gamification.routing;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import mmmi.sdu.dk.gamification.utils.JSONParser;
+
+public class GetDirectionsAsync extends AsyncTask<LatLng, Void, List<LatLng>> {
+      GoogleMap mMap;
+      Polyline line;
+
+      JSONParser jsonParser;
+      String DIRECTIONS_URL = "https://maps.googleapis.com/maps/api/directions/json";
+
+
+      public GetDirectionsAsync(GoogleMap map) {
+            this.mMap = map;
+      }
+
+      @Override
+      protected void onPreExecute() {
+            super.onPreExecute();
+      }
+
+      @Override
+      protected List<LatLng> doInBackground(LatLng... params) {
+            LatLng start = params[0];
+            LatLng end = params[1];
+
+            HashMap<String, String> points = new HashMap<>();
+            points.put("origin", start.latitude + "," + start.longitude);
+            points.put("destination", end.latitude + "," + end.longitude);
+
+            jsonParser = new JSONParser();
+
+            JSONObject obj = jsonParser.makeHttpRequest(DIRECTIONS_URL, "GET", points, true);
+
+            if (obj == null) return null;
+
+            try {
+                  List<LatLng> list = null;
+
+                  JSONArray routeArray = obj.getJSONArray("routes");
+                  JSONObject routes = routeArray.getJSONObject(0);
+                  JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+                  String encodedString = overviewPolylines.getString("points");
+                  list = decodePoly(encodedString);
+
+                  return list;
+
+            } catch (JSONException e) {
+                  e.printStackTrace();
+            }
+
+            return null;
+      }
+
+      @Override
+      protected void onPostExecute(List<LatLng> pointsList) {
+
+            if (pointsList == null) return;
+
+            if (line != null){
+                  line.remove();
+            }
+
+            PolylineOptions options = new PolylineOptions().width(5).color(Color.MAGENTA).geodesic(true);
+            for (int i = 0; i < pointsList.size(); i++) {
+                  LatLng point = pointsList.get(i);
+                  options.add(point);
+            }
+            line = mMap.addPolyline(options);
+
+      }
+
+      private List<LatLng> decodePoly(String encoded) {
+
+            List<LatLng> poly = new ArrayList<LatLng>();
+            int index = 0, len = encoded.length();
+            int lat = 0, lng = 0;
+
+            while (index < len) {
+                  int b, shift = 0, result = 0;
+                  do {
+                        b = encoded.charAt(index++) - 63;
+                        result |= (b & 0x1f) << shift;
+                        shift += 5;
+                  } while (b >= 0x20);
+                  int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                  lat += dlat;
+
+                  shift = 0;
+                  result = 0;
+                  do {
+                        b = encoded.charAt(index++) - 63;
+                        result |= (b & 0x1f) << shift;
+                        shift += 5;
+                  } while (b >= 0x20);
+                  int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                  lng += dlng;
+
+                  LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+                  poly.add(p);
+            }
+
+            return poly;
+      }
+}
